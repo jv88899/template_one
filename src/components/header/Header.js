b86e84013db0fc040c319c47d1c46bd5ceb65d28
jv88899 <jv88899@gmail.com>
2019-10-15 21:14:13 -0500

    added nav links functionality

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,40 +1,56 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './header.css'
 
-const Header = () => (
-    <header className="header">
-        <nav className="header__nav">
-            <div className="header__logo">
-                <div className="header__logo__img">
-                    <p>&#60; &#47; &#62;</p>
-                </div>
-                <div className="header__logo__text">
-                    <span>JVO</span>
-                    <span
-                        className="header__logo__text__hidden"
+class Header extends Component {
+    state = {
+        navLinksVisible: false
+    }
+
+    handleMenuClick = () => {
+        this.setState({ navLinksVisible: !this.state.navLinksVisible })
+    }
+
+    render() {
+        let navLinksClass = 'header__links__list'
+        this.state.navLinksVisible ? navLinksClass = 'header__links__list visible' : navLinksClass = 'header__links__list'
+
+        return (
+            <header className="header">
+                <nav className="header__nav">
+                    <div className="header__logo">
+                        <div className="header__logo__img">
+                            <p>&#60; &#47; &#62;</p>
+                        </div>
+                        <div className="header__logo__text">
+                            <span>JVO</span>
+                            <span
+                                className="header__logo__text__hidden"
+                            >
+                                Javascript Developer
+                            </span>
+                        </div>
+                    </div>
+                    <div className="header__links">
+                        <ul className={navLinksClass}>
+                            <li>About</li>
+                            <li>Portfolio</li>
+                            <li>Testimonials</li>
+                            <li>Contact</li>
+                            <li>Hire Me!</li>
+                        </ul>
+                    </div>
+                    <div
+                        className="header__menu"
+                        onClick={this.handleMenuClick}
                     >
-                        Javascript Developer
-                    </span>
-                </div>
-            </div>
-            <div className="header__links">
-                <ul>
-                    <li>About</li>
-                    <li>Portfolio</li>
-                    <li>Testimonials</li>
-                    <li>Contact</li>
-                    <li>Hire Me!</li>
-                </ul>
-            </div>
-            <div
-                className="header__menu"
-            >
-                <div className="header__menu__bar"></div>
-                <div className="header__menu__bar"></div>
-                <div className="header__menu__bar"></div>
-            </div>
-        </nav>
-    </header>
-)
+                        <div className="header__menu__bar"></div>
+                        <div className="header__menu__bar"></div>
+                        <div className="header__menu__bar"></div>
+                    </div>
+                </nav>
+            </header>
+        )
+    }
+}
 
 export default Header
