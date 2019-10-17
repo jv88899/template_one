e3c944d4ca9b5fe50003500f864a58a2b65f7bf6
jv88899 <jv88899@gmail.com>
2019-10-16 19:37:36 -0500

    added to header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -17,37 +17,49 @@ class Header extends Component {
         return (
             <header className="header">
                 <nav className="header__nav">
-                    <div className="header__logo">
-                        <div className="header__logo__img">
-                            <p>&#60; &#47; &#62;</p>
+                    <div className="header__nav__wrapper">
+                        <div className="header__logo">
+                            <div className="header__logo__img">
+                                <p>&#60; &#47; &#62;</p>
+                            </div>
+                            <div className="header__logo__text">
+                                <span>JVO</span>
+                                <span
+                                    className="header__logo__text__hidden"
+                                >
+                                    Javascript Developer
+                                </span>
+                            </div>
                         </div>
-                        <div className="header__logo__text">
-                            <span>JVO</span>
-                            <span
-                                className="header__logo__text__hidden"
-                            >
-                                Javascript Developer
-                            </span>
+                        <div className="header__links">
+                            <ul className={navLinksClass}>
+                                <li>About</li>
+                                <li>Portfolio</li>
+                                <li>Testimonials</li>
+                                <li>Contact</li>
+                                <li>Hire Me!</li>
+                            </ul>
                         </div>
-                    </div>
-                    <div className="header__links">
-                        <ul className={navLinksClass}>
-                            <li>About</li>
-                            <li>Portfolio</li>
-                            <li>Testimonials</li>
-                            <li>Contact</li>
-                            <li>Hire Me!</li>
-                        </ul>
-                    </div>
-                    <div
-                        className="header__menu"
-                        onClick={this.handleMenuClick}
-                    >
-                        <div className="header__menu__bar"></div>
-                        <div className="header__menu__bar"></div>
-                        <div className="header__menu__bar"></div>
+                        <div
+                            className="header__menu"
+                            onClick={this.handleMenuClick}
+                        >
+                            <div className="header__menu__bar"></div>
+                            <div className="header__menu__bar"></div>
+                            <div className="header__menu__bar"></div>
+                        </div>
                     </div>
                 </nav>
+                <div className="header__information">
+                    <h1>Hi! I'm Jeremy.</h1>
+                    <h2>I make websites that help organizations and businesses grow.</h2>
+                    <a
+                        href="/"
+                        className="header__portfolio__button"
+                    >
+                        View Portfolio
+                    </a>
+                </div>
             </header>
         )
     }
